2720b6aec7c9d90c7e26d6d980bba8abf1c733ea
hitesharora1997 <hitesh.arora1997@gmail.com>
2024-01-02 15:12:00 +0100

    tress in rust

--- dsa/trees/trees.cpp
+++ dsa/trees/trees.cpp
@@ -1,18 +1,29 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <thread>
+#include <vector>
 
-class node {
-    int data;
-    node *left;
-    node *right;
+constexpr int size = 1000000;
+std::vector<int> data(size, 1);  // Array of 1s
 
-    node(int d) {
-        this->data = d;
-        this->left = NULL;
-        this->right = NULL;
-    };
-};
+int result = 0;
+
+void calculate_sum(int start, int end) {
+    for (int i = start; i < end; ++i) {
+        result += data[i];
+    }
+}
 
 int main() {
+    // Create two threads
+    std::thread thread1(calculate_sum, 0, size / 2);
+    std::thread thread2(calculate_sum, size / 2, size);
+
+    // Wait for both threads to finish
+    thread1.join();
+    thread2.join();
+
+    // Print the result
+    std::cout << "Sum: " << result << std::endl;
+
     return 0;
 }
